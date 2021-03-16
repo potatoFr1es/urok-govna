acb8dcd71830193a1316dc52150c4d0625036417
potatoFr1es <torbenborstl@gmail.com>
2021-03-16 12:34:22 +0300

    задача на массивы номер 2

--- Program.cs
+++ Program.cs
@@ -6,50 +6,25 @@ namespace задачи
     {
         static void Main(string[] args)
         {
-          //здаание эл-ов массива при его описании 
-          //многомерные массивы
-        //   int[,] a = new int[4,2];
-        
-          //даны натуральные n целых чисел a1 an все числа попарно различны поменять в последовательности местами наибольший и наименьший члены
-          byte n;
-          int[] a;
-          int max, min;
-          byte i, nom_max, nom_min;
-          Console.WriteLine("Input n");
-          n = byte.Parse(Console.ReadLine());
-          if (n == 0) return;
-          a = new int[n];
-          for (i = 0; i< n; i++)
-          {
-            Console.Write("Input a[{0}]",i);
-            a[i] = int.Parse(Console.ReadLine());
-          }
-          min = a[0];
-          max = a[0];
-          nom_max = 0;
-          nom_min = 0;
-          for (i=1;i<n;i++)
-          {
-              if(a[i]<min)
-              {
-                  min = a[i];
-                  nom_min = i;
-              }
-              if(a[i]>max)
-              {
+            // ввести массив a из 6-ти эл-ов найти сумму и кол-во отрицательных а так же максимальный элемент
+            const  int n = 6;
+            int sum = 0;
+            int sum_min = 0;
+            int max = 0;
+            int[] a = new int[n];
+            foreach(int i  in a)
+            {
+                a[i] = Int32.Parse(Console.ReadLine());
+                sum = sum + a[i];
+                if (a[i] < 0)
+                sum_min = sum_min + a[i];
+                if(i == 0)
                 max = a[i];
-                nom_max = i;
-              }
-
-          }
-           a[nom_min] = max;
-           a[nom_max] = min;
-           for (i = 0; i<n;i++)
-           {
-               Console.Write(a[i]+" ");
-           }
-
-          Console.ReadKey();
+                if (a[i] > max)
+                max = a[i];
+            }
+            Console.WriteLine("сумма элементов равна {0}, сумма отрицательных равно {1} максимальное число равно {2}",sum,sum_min,max);
+            Console.ReadKey();
 
 
         }
