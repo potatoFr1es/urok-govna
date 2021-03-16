58fb4499779e605b5d2498f1cd795e1d18b8a98b
potatoFr1es <torbenborstl@gmail.com>
2021-03-16 11:49:23 +0300

    urok huini

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //здаание эл-ов массива при его описании 
+          //многомерные массивы
+          
+        }
+    }
+}
